2ac703659136672693ec46516b7e7003b5acb40e
Benjamin <Ben@benjamins-imac.carolina.rr.com>
2015-12-23 13:04:11 -0500

    Initial Commit

---
+++ Voice Manipulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voice Manipulator
+//
+//  Created by Benjamin on 12/23/15.
+//  Copyright © 2015 BenjaminVanHouten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Voice ManipulatorTests/Voice_ManipulatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Voice_ManipulatorTests.swift
+//  Voice ManipulatorTests
+//
+//  Created by Benjamin on 12/23/15.
+//  Copyright © 2015 BenjaminVanHouten. All rights reserved.
+//
+
+import XCTest
+@testable import Voice_Manipulator
+
+class Voice_ManipulatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
